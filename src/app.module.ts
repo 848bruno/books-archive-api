8bb1edf7ef0e9fdb62d58b53fb3cb977792b0f00
848bruno <ambalebruno848@gmail.com>
2025-05-31 21:07:28 +0300

    updated database.module.ts confugaration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,43 +1,17 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './user/user.module';
 import { ProfileModule } from './profile/profile.module';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
 import { CategoryModule } from './category/category.module';
 import { BookreviewModule } from './bookreview/bookreview.module';
-import { DatabaseModule } from './database/database.module';
 import { SeedModule } from './seed/seed.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true,
-      envFilePath: '.env',
-     }), 
-    TypeOrmModule.forRootAsync({
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService) => ({
-    type: 'postgres',
-    host: configService.getOrThrow('DB_HOST'),
-    port: configService.get<number>('DB_PORT') ?? 5432,
-    username: configService.getOrThrow('DB_USERNAME'),
-    password: configService.getOrThrow('DB_PASSWORD'),
-    database: configService.getOrThrow('DB_NAME'),
-    autoLoadEntities: true,
-    synchronize: true,
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false, 
-      },
-    },
-
-
-      }),
-    }),
-    DatabaseModule,
+    DatabaseModule, // handles TypeORM config
     UsersModule,
     ProfileModule,
     AuthorModule,
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,27 +1,37 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.getOrThrow<string>('DB_HOST'),
-        port: configService.getOrThrow<number>('DB_PORT'),
-        username: configService.getOrThrow<string>('DB_USERNAME'),
-        password: configService.getOrThrow<string>('DB_PASSWORD'),
-        database: configService.getOrThrow<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: configService.get<boolean>('DB_SYNC') ?? true,
-        logging: configService.get<boolean>('DB_LOGGING') ?? false,
-        ssl: true, 
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
-      }),
-      inject: [ConfigService],
+        inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+        const sslEnabled = configService.get('DB_SSL') === 'true';
+        const dbConfig: TypeOrmModuleOptions = {
+          type: 'postgres',
+          host: configService.getOrThrow<string>('DB_HOST'),
+          port: configService.getOrThrow<number>('DB_PORT'),
+          username: configService.getOrThrow<string>('DB_USERNAME'),
+          password: configService.getOrThrow<string>('DB_PASSWORD'),
+          database: configService.getOrThrow<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: configService.get<boolean>('DB_SYNC') ?? true,
+          logging: configService.get<boolean>('DB_LOGGING') ?? false,
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+          ssl: sslEnabled ? { rejectUnauthorized: false } : false,
+        };
+
+        return dbConfig;
+      },
     }),
   ],
 })
